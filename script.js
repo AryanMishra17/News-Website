5edea085b0285a8de2309fd3277507bc4146ccd9
AryanMishra17 <151031618+AryanMishra17@users.noreply.github.com>
2025-01-30 11:48:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+const API_KEY ="c22d71e47661428cb203f07261b3b381";
+const url ="https://newsapi.org/v2/everything?q=";
+
+
+window.addEventListener("load", () => fetchNews("India"));
+
+function reload() {
+    window.location.reload();
+}
+
+async function fetchNews(query) {
+    const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data = await res.json();  //take data in json formate
+    bindData(data.articles);  
+}
+
+function bindData(articles) {
+    const cardsContainer = document.getElementById("cards-container");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML = "";  // new search then clear the previous card
+
+    articles.forEach((article) => {
+        if (!article.urlToImage) return;  // if not imge present in api then not display such news
+        const cardClone = newsCardTemplate.content.cloneNode(true);  //template tag completely clone with all div present in it. 
+        fillDataInCard(cardClone, article);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+
+function fillDataInCard(cardClone, article) {
+
+    const newsImg = cardClone.querySelector("#news-img");
+    const newsTitle = cardClone.querySelector("#news-title");
+    const newsSource = cardClone.querySelector("#news-source");
+    const newsDesc = cardClone.querySelector("#news-desc");
+ // changing card data by api data
+    newsImg.src = article.urlToImage;
+    newsTitle.innerHTML = article.title;
+    newsDesc.innerHTML = article.description;
+// img title source describtion then add date 
+// for date api give in time zone formate makeit into human readable form by using this library in js new date()
+    const date = new Date(article.publishedAt).toLocaleString("en-US", {
+        timeZone: "Asia/Jakarta",
+    });
+
+    newsSource.innerHTML = `${article.source.name} · ${date}`;
+
+
+    //when click on card then open article url present in api
+    cardClone.firstElementChild.addEventListener("click", () => {
+        window.open(article.url, "_blank");
+    });
+}
+
+let curSelectedNav = null;
+function onNavItemClick(id) {
+    fetchNews(id);
+    const navItem = document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton = document.getElementById("search-button");
+const searchText = document.getElementById("search-text");
+
+searchButton.addEventListener("click", () => {
+    const query = searchText.value;
+    if (!query) return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav = null;
+});
+
